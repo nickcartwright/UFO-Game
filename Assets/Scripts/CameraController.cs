ddd26ff5c4ef681951d644ee018ffd685248e43f
Nick Cartwright <necartwright@gmail.com>
2018-09-24 22:30:12 +0100

    A few more lessons..

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        // transform.positon references the position of the camera
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Gauranteed to be called after update
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour {
+	
+	// Time.deltaTime....
+    // Vector 3 as transform still exists in 3d volume.
+	void Update () {
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+	}
+}
